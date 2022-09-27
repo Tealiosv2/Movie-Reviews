a53591069793bcb96d29043a57dcf3ba68f86885
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2022-09-27 00:46:24 -0700

    Revert "Merge branch 'dev'"
    This reverts commit 27b852757a3365494f302509d2e6990705798087, reversing
    changes made to 2cf1fd4f3e510c4fc676d8de2fc42ba3335a0736.

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,2 @@
 //scripting for main.html
 
-var one = 0;
-    function click1(){
-      one = one + 1;
-      if (one == 3){
-        window.location.href= "https://cdn.shopify.com/s/files/1/0254/0516/1520/products/01_DysonSphere_Rework_Front.jpg?v=1649160313&width=2048";
-        one = 0;
-        }
-    }
-
