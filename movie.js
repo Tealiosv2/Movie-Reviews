a4e1cb96ff69523ac3bb09009e0606376c3dc456
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2022-09-28 12:40:57 -0700

    added navbar to stuff

--- movie.js
+++ movie.js
@@ -17,7 +17,7 @@ app.get("/", function(req, res) {
   });
 
   //returns howl's moving castle page
-  app.get("/HMC-review", function(req, res) {
+  app.get("/HMC", function(req, res) {
     res.send(fs.readFileSync("./public/html/HMC-review.html", "utf8"));
   });
 
